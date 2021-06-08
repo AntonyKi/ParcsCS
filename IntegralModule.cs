6c5a7c939585444ee9b7cd3e9d0256b07e007172
unknown <antony.kisil.o@gmail.com>
2021-06-08 21:43:11 +0300

    initial commit

---
+++ IntegralModule.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Drawing;
+using System.Threading;
+using Parcs;
+
+namespace FirstModule
+{
+    public class IntegralModule: IModule
+    {
+
+
+        private static Bitmap Blur(Bitmap image, Int32 blurSize, Int32 partOfImage, Int32 total)
+        {
+            int widthOfPart = image.Width / total;
+            int x = partOfImage*widthOfPart;
+            //Console.WriteLine(x);
+            if (x > blurSize)
+            {
+                x -= blurSize;
+                widthOfPart += blurSize;
+            }
+            return Blur(image, new Rectangle(x, 0, widthOfPart, image.Height), blurSize);
+        }
+
+        private static Bitmap Blur(Bitmap image, Rectangle rectangle, Int32 blurSize)
+        {
+            Bitmap blurred = new Bitmap(image.Width, image.Height);
+
+            using (Graphics graphics = Graphics.FromImage(blurred))
+                graphics.DrawImage(image, new Rectangle(0, 0, image.Width, image.Height),
+                    new Rectangle(0, 0, image.Width, image.Height), GraphicsUnit.Pixel);
+            Console.WriteLine(rectangle.X + " " + rectangle.Y);
+            for (int xx = rectangle.X; xx < rectangle.X + rectangle.Width; xx++)
+            {
+                for (int yy = rectangle.Y; yy < rectangle.Y + rectangle.Height; yy++)
+                {
+                    int avgR = 0, avgG = 0, avgB = 0;
+                    int blurPixelCount = 0;
+                    for (int x = xx; (x < xx + blurSize && x < image.Width); x++)
+                    {
+                        for (int y = yy; (y < yy + blurSize && y < image.Height); y++)
+                        {
+                            Color pixel = blurred.GetPixel(x, y);
+
+                            avgR += pixel.R;
+                            avgG += pixel.G;
+                            avgB += pixel.B;
+
+                            blurPixelCount++;
+                        }
+                    }
+
+                    avgR = avgR / blurPixelCount;
+                    avgG = avgG / blurPixelCount;
+                    avgB = avgB / blurPixelCount;
+
+                    for (int x = xx; x < xx + blurSize && x < image.Width; x++)
+                        for (int y = yy; y < yy + blurSize && y < image.Height; y++)
+                            blurred.SetPixel(x, y, Color.FromArgb(avgR, avgG, avgB));
+                }
+            }
+
+            return blurred;
+        }
+
+        public void Run(ModuleInfo info, CancellationToken token = default(CancellationToken))
+        {
+            Bitmap image = info.Parent.ReadObject<Bitmap>();
+            int blurSize = info.Parent.ReadInt();
+            int partOfImage = info.Parent.ReadInt();
+            int totalParts = info.Parent.ReadInt();
+
+            Bitmap res = Blur(image, blurSize, partOfImage, totalParts);
+            Console.WriteLine("Finished");
+            info.Parent.WriteObject(res);
+        }
+    }
+}
---
+++ MainIntegralModule.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Drawing;
+using System.Reflection;
+using System.Threading;
+using Parcs;
+using System.Linq;
+
+namespace FirstModule
+{
+    class MainIntegralModule: IModule
+    {
+        public static void CopyRegionIntoImage(Bitmap srcBitmap, Rectangle srcRegion, ref Bitmap destBitmap, Rectangle destRegion)
+        {
+            using (Graphics grD = Graphics.FromImage(destBitmap))
+            {
+                grD.DrawImage(srcBitmap, destRegion, srcRegion, GraphicsUnit.Pixel);
+            }
+        }
+        public static void Main(string[] args)
+        {
+            
+            var job = new Job();
+            if (!job.AddFile(Assembly.GetExecutingAssembly().Location/*"MyFirstModule.exe"*/))
+            {
+                Console.WriteLine("File doesn't exist");
+                return;
+            }
+
+            new MainIntegralModule().Run(new ModuleInfo(job, null));
+            Console.ReadKey();
+        }
+
+        public void Run(ModuleInfo info, CancellationToken token = default(CancellationToken))
+        {
+            Bitmap bitmap = new Bitmap("input.png");
+            var pointsNum = 1;
+            var points = new IPoint[pointsNum];
+            var channels = new IChannel[pointsNum];
+            var bmres = new Bitmap(bitmap);
+            int widthOfPart = bitmap.Width / pointsNum;
+            Console.WriteLine(widthOfPart);
+            int h = bitmap.Height;
+            for (int i = 0; i < pointsNum; ++i)
+            {
+                points[i] = info.CreatePoint();
+                channels[i] = points[i].CreateChannel();
+                points[i].ExecuteClass("FirstModule.IntegralModule");
+            }
+
+            
+            for (int i = 0; i < pointsNum; ++i)
+            {
+                channels[i].WriteObject(new Bitmap(bitmap));
+                channels[i].WriteData(5);
+                channels[i].WriteData(i);
+                channels[i].WriteData(pointsNum);
+            }
+            DateTime time = DateTime.Now;            
+            Console.WriteLine("Waiting for result...");
+            
+            var bmps = channels.Select(c => new Lazy<Bitmap>(c.ReadObject<Bitmap>)).ToArray();
+            
+            Console.WriteLine("Blurred time = {0}", Math.Round((DateTime.Now - time).TotalSeconds,3));
+
+            
+            for (int i = 0; i < pointsNum; ++i)
+            {
+                int x = i * widthOfPart;
+                var region = new Rectangle(x, 0, widthOfPart, h);
+                Console.WriteLine("merge close to done");
+                //bmps[i].Value.Save("output" + ".png");
+                CopyRegionIntoImage(bmps[i].Value, region, ref bmres, region);
+                //Console.WriteLine(bmps[i].Value.Height);
+            }
+            bmres.Save("output.png");
+            Console.WriteLine("Merged time = {0}", Math.Round((DateTime.Now - time).TotalSeconds, 3));
+        }
+    }
+}
